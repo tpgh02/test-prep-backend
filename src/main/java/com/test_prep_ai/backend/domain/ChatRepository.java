5f5396430ccbb977aa56cf62c97a296dd846344d
tpgh02 <seaho4588@gmail.com>
2025-02-26 14:39:38 +0900

    init

--- src/main/java/com/test_prep_ai/backend/domain/ChatRepository.java
+++
@@ -1,6 +0,0 @@
-package com.test_prep_ai.backend.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ChatRepository extends CrudRepository<Chat, Long> {
-}
--- src/main/java/com/test_prep_ai/backend/web/ChatController.java
+++
@@ -1,9 +0,0 @@
-package com.test_prep_ai.backend.web;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/chat")
-public class ChatController {
-}
