3d7eec86db162ad8b4293169c38331736d4106c2
tpgh02 <seaho4588@gmail.com>
2025-02-27 11:19:48 +0900

    [fix] multipartfile을 Dto로 넣어서 발생한 오류 수정

--- src/main/java/com/test_prep_ai/backend/question/service/QuestionService.java
+++ src/main/java/com/test_prep_ai/backend/question/service/QuestionService.java
@@ -3,11 +3,12 @@ package com.test_prep_ai.backend.question.service;
 import com.test_prep_ai.backend.question.domain.QuestionEntity;
 import com.test_prep_ai.backend.question.dto.QuestionRequest;
 import com.test_prep_ai.backend.question.dto.QuestionResponse;
-import com.test_prep_ai.backend.security.Session;
 import com.test_prep_ai.backend.util.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +16,7 @@ public class QuestionService {
 
     private final S3Service s3Service;
 
-    public void createQuestionRequest(QuestionRequest questionRequest, Session session) {
+    public void createQuestionRequest(QuestionRequest questionRequest) {
         QuestionEntity.builder()
                 .level(questionRequest.getLevel())
                 .types(questionRequest.getTypes())
@@ -27,11 +28,11 @@ public class QuestionService {
         return s3Service.uploadFile(file);
     }
 
-    public QuestionResponse createQuestionResponse(QuestionRequest questionRequest, String fileName) {
+    public QuestionResponse createQuestionResponse(List<Integer> types, String level, String message, String fileName) {
         return QuestionResponse.builder()
-                .level(questionRequest.getLevel())
-                .types(questionRequest.getTypes())
-                .message(questionRequest.getMessage())
+                .level(level)
+                .types(types)
+                .message(message)
                 .fileName(fileName)
                 .build();
     }
